a34ae127b86f9334a021593d0a8b35f796a7b0d0
ramonjesus89 <58955796+ramonjesus89@users.noreply.github.com>
2024-06-05 14:16:06 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em (alterado github) http://localhost:8000")
+  console.log("API disponível em (alterado github - nova tentativa de conflito) http://localhost:8000")
 })
