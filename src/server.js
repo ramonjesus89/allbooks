7e934d580703a9ecc8e0a274fd40738538953a7d
ramonjesus89 <ramomjesus@gmail.com>
2024-06-05 14:40:53 -0300

    altera o server.js CONFLITO

--- src/server.js
+++ src/server.js
@@ -18,5 +18,10 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
+<<<<<<< HEAD
   console.log("API disponível em (alterado github - nova tentativa de conflito) http://localhost:8000")
+=======
+  console.log("Boas-vindas à API do AllBooks")
+  console.log("API disponível em (alterado github conflito 2) http://localhost:8000")
+>>>>>>> 4b26625 (altera o server.js CONFLITO)
 })
